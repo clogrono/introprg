a35b31940ba8ee9d6751394e743153056e0e36b7
Moisès <moisesgomezgiron@gmail.com>
2020-03-20 20:54:30 +0100

    Esquelet del primer programa per què Christian pugui començar ;)

---
+++ 07_01_fitxapersona/InformePersona.java
@@ -0,0 +1,12 @@
+/*
+ * Aquí has de crear la classe que se't demana
+ * Et poso l'esquema per ser la primera
+ *
+ * Un cop el tinguis, des de la línia de comandes, tecleja
+ * bash test/test.sh
+ *
+ * Segueix les instruccions
+ */
+public class InformePersona {
+    // XXX TODO: està tot per fer!
+}
