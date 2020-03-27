89c7d5a4c3d1469d75b55915b654a753f24b7c28
clogrono <cf19christian.logrono@iesjoandaustria.org>
2020-03-27 20:20:14 +0100

    Exercici 07_02_fitxespersones aconseguit

---
+++ 07_02_fitxespersones/InformePersona.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class InformePersona {
+    
+    static String composaFitxa(String nom,String cognoms , int edat) {
+        return String.format(
+            "Fitxa\n=====\nNom: %s %s\nEdat: %d %s\n",
+                nom,
+                cognoms, 
+                edat,
+                edat > 1 ? "anys" : "any"
+        );
+    }
+    static String composaFitxes(String[] noms, String[] cognoms, int[] edats){
+	String resultat = "";
+        for (int i=0; i < noms.length; i++) {
+            resultat = String.format("%s%s", resultat, composaFitxa(noms[i],cognoms[i], edats[i]));
+        }
+        return resultat;
+    }
+
+    public static void main(String[] args) {
+        //  String nom;
+        //  String cognoms;
+        //  int edat;
+        //  Scanner dadesIntro =new Scanner(System.in);
+        //  System.out.println("Quin és el teu nom?");
+        //  nom=dadesIntro.nextLine();
+        //  System.out.println("Quins són els teus cognoms?");
+        //  cognoms=dadesIntro.nextLine();
+        //  System.out.println("Quina és la teva edat?");
+        //  edat=dadesIntro.nextInt();
+     
+        String[] noms = {"Laia"};
+        String[] cognoms = {"Sànchez d'Àrgent"};
+        int[] edats = {19};
+
+        String missatge  = composaFitxes(noms,cognoms, edats);
+        System.out.println(missatge);
+    }
+}
