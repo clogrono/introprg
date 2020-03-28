4edae51fcf1bd92d6de191f6540bbc0a28ac72a9
clogrono <cf19christian.logrono@iesjoandaustria.org>
2020-03-28 16:41:27 +0100

    Excercici 08_10_estatprotegit superat

---
+++ 08_10_estatprotegit/GatRenat.java
@@ -0,0 +1,90 @@
+public class GatRenat {
+   
+    GatEstat gatEstat;
+    int vides = 7;
+    String estat = "estirat";
+
+    public String aixecat(){
+        if(estat != "dret"){
+            this.estat = "dret";
+            return "m'aixeco";
+        }
+        else return"no faig res";
+    }
+
+    public String seu(){
+        if(estat != "assegut"){
+            this.estat = "assegut";
+            return "m'assec";
+        }
+        else return"no faig res";
+    }
+
+    public String estirat(){
+        if(estat != "estirat"){
+            this.estat = "estirat";
+            return "m'estiro";
+        }
+        else return"no faig res";
+    }
+
+    public boolean estaViu(){
+        return vides > 0 ? true : false ;
+    }
+
+    public boolean estaDret(){
+        return estat == "dret" ? true : false ;
+    }
+
+    public boolean estaAssegut(){
+        return estat == "assegut" ? true : false ;
+    }
+
+    public boolean estaEstirat(){
+        return estat == "estirat" ? true : false ;
+    }
+    
+    public GatEstat getEstat() { 
+        return gatEstat;
+    }
+
+    public String getEstatComString() { 
+        return estat;
+    }
+
+
+    public void setEstat(String estat) {
+        if (estat =="dret"||estat =="assegut"||estat =="estirat") {
+            this.estat = estat;   
+        }
+    }
+
+    public void setEstat(GatEstat gatEstat) {
+        if (gatEstat == gatEstat.DRET ||gatEstat == GatEstat.ASSEGUT ||gatEstat == GatEstat.ESTIRAT) {
+            this.gatEstat = gatEstat; 
+             
+        }
+    }
+
+    public int getVides(){ 
+        return vides;
+    }
+    public void setVides(int vides) {
+        if (vides >= 0) {
+            this.vides = vides;  
+        }
+    }
+
+    public static void main(String[] args) {
+        GatRenat renat = new GatRenat();  
+        // System.out.println("Al gat Renat li queden " + renat.vides + " vides y esta "+renat.getEstat());
+        
+        System.out.println(renat.aixecat());
+       
+        System.out.println(renat.seu());
+     
+        System.out.println(renat.estirat());
+        
+     }
+}
+
---
+++ 08_10_estatprotegit/UsaGatRenat.java
@@ -0,0 +1,24 @@
+public class UsaGatRenat {
+    public static void main(String[] args) {
+        GatRenat renat = new GatRenat();  
+        System.out.println("Al gat Renat li queden " + renat.vides + " vides y esta "+renat.getEstatComString());
+        System.out.println("Al gat Renat li queden " + renat.vides + " vides y esta "+renat.getEstat());
+ 
+        System.out.println(renat.aixecat());
+       
+        System.out.println(renat.seu());
+     
+        System.out.println(renat.estirat()); 
+
+        renat.setEstat(GatEstat.ESTIRAT);
+        System.out.println("Al gat Renat li queden " + renat.vides + " vides y esta "+renat.getEstat());
+
+        renat.setEstat(GatEstat.ASSEGUT);
+        renat.setEstat(GatEstat.DRET);
+
+    
+    
+    }
+
+        
+}
