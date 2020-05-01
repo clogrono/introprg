7d2d8342cbb35485c26545a40fce6e8ab5c8a71e
clogrono <cf19christian.logrono@iesjoandaustria.org>
2020-05-01 22:51:02 +0200

    Exercici 09_06_capicuatests superat

--- 09_06_capicuatests/capicuatest/src/main/java/Main.java
+++ 09_06_capicuatests/capicuatest/src/main/java/Main.java
@@ -9,8 +9,25 @@ import static spark.Spark.get;
 public class Main {
 
     static String composaSalutacio(String nom) {
-          if (nom == null) {
+        if (nom == null) {
             nom = "anna";
+        }else{
+            String capicua="";
+            for(int x = nom.length() -1; x >= 0; x--) {
+                char c = nom.charAt(x);
+                capicua=capicua+c;
+            }
+
+            if (nom.equals(capicua)){
+                nom=capicua;
+            }else{
+                String aux="";
+                for(int i = nom.length() -1; i > 0; i--) {
+                    char ch = nom.charAt(i);
+                    aux=aux+ch;
+                }
+                nom=aux+nom;
+            }
         }
         return "La paraula capicua es <b>"+nom+"</b>";
     }
